24d90a7ca7a44c81b8ffc762fa8ec2e27bde672f
Hal Henke <halhenke@gmail.com>
2016-08-26 23:12:52 +1000

    Trying to get something to work from Thermite

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,17 @@ module Main where
 
 import Prelude
 import Data.AddressBook
+import UI.Counter
+import React as R
+import React.DOM as RD
+import React.DOM.Props as RDP
+import ReactDOM as RDOM
+import Thermite as T
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (logShow, CONSOLE, log)
+import React (ReactProps, getProps)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall eff. Eff (console :: CONSOLE | eff) Unit
 main = do
   log "Hello sailor!"
   -- log (showEntry eddy) where
@@ -18,3 +25,15 @@ main = do
       -- log ("fuck you")
       -- logShow showAddress addy
   -- log ("Monkey")
+
+  -- RD.body >>= RDOM.render (R.createFactory (component { apple: "bee"}))
+  -- let a = {b: "pop"} :: ReactProps
+  -- let a :: State
+  -- let a = {counter: 23} :: State
+  -- RD.body >>= RDOM.render (R.createFactory initialState)
+  -- RD.body >>= RDOM.render (R.createFactory (component a)) where
+  -- a = {counter: 2 :: Int}
+  -- RD.body >>= RDOM.render (R.createFactory (component {counter: 23}))
+
+  let component = (T.createClass spec initialState)
+  RD.body >>= RDOM.render (R.createFactory (component {}))
